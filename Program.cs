0f7f203fcbf2f82c841e4cc213969082e1232bdb
Manomano Takudzwa Danmore <OM014299@oldmutual.local>
2018-06-29 14:59:06 +0200

    Initial Creation of the project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myFirstRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
